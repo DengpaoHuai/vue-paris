a7d3dc62cc42829270c4fe18f32f869ffb92afeb
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-06-17 11:50:14 +0200

    init

---
+++ src/components/CardDemo.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+    title: string,
+    footer: string
+}>()
+</script>
+
+<template>
+    <section>
+        <section>
+            <h2>{{ title }}</h2>
+            <p>Here is a card demo</p>
+        </section>
+        <slot></slot>
+
+        <section>
+            <p>
+                {{ footer }}
+            </p>
+        </section>
+    </section>
+</template>
---
+++ src/components/DemoCounter.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+const counter = ref(0);
+const incrementCounter = () => {
+    counter.value++;
+};
+onMounted(() => {
+    console.log('ok')
+})
+</script>
+
+<template>
+    <p>
+        count : {{ counter }}
+    </p>
+    <button @click="incrementCounter">increment</button>
+</template>
